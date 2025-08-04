6b95c55505052242d14516eeeda9148884a01326
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-04 21:56:55 +0800

    20250804 v1

---
+++ open-and-git.bat
@@ -0,0 +1,9 @@
+cd "C:\Users\chbon\Documents\zolaond3aak3a"
+git init
+git add .
+git commit -m "20250804 v1"
+git remote add origin https://github.com/ond3aak3a/ond3aa3a.github.io.git
+git checkout -b main
+git push -u origin main --force
+explorer  http://localhost:1111
+zola serve
